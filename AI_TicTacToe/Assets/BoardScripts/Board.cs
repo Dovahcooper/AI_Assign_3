6919d4dab258cb8389d3bc63110bc07067c47ebc
Dovahcooper <matthew.holt@ontariotechu.net>
2020-02-22 22:39:02 -0500

    [ADD] Tic-Tac-Toe basic
    can place Xs in the scene
    only took 2 hours of constant breakdowns, but it's there

---
+++ AI_TicTacToe/Assets/BoardScripts/Board.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public int[,] boardMatrix; //values of 0 will be empty spaces, 1 will be X, 2 will be O. This was the easiest way I could think of to make it readable to an AI
+
+    public Tile left, right, top, bot, centre, tLeft, bLeft, tRight, bRight;
+    public Tile[,] boardTiles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boardMatrix = new int[3, 3] { { 0, 0, 0 },
+            { 0, 0, 0},
+            {0, 0, 0 } };
+
+        boardTiles = new Tile[3, 3] { { tLeft, top, tRight },
+        { left, centre, right },
+        { bLeft, bot, bRight }};
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetMatrix(int row, int col, int type)
+    {
+        boardMatrix[row, col] = type;
+    }
+}
---
+++ AI_TicTacToe/Assets/BoardScripts/Tile.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class Tile : MonoBehaviour
+{
+    [SerializeField] UnityEvent onClickEvent;
+
+    Board theBoard;
+    public Vector2Int arrayPos;
+
+    MeshRenderer thisRenderer;
+    public Material xMat, oMat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector2Int tempPos = new Vector2Int((int)(transform.position.x), (int)(transform.position.y));
+        tempPos /= 2;
+        theBoard = GetComponentInParent<Board>();
+        arrayPos = new Vector2Int((tempPos.y - 1) * -1, tempPos.x + 1);
+        thisRenderer = GetComponent<MeshRenderer>();
+
+        onClickEvent.AddListener(SelectTileX);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        onClickEvent.Invoke();
+    }
+
+    public void SelectTileX()
+    {
+        thisRenderer.material = xMat;
+        theBoard.SetMatrix(arrayPos.x, arrayPos.y, 1);
+    }
+
+    public void SelectTileO()
+    {
+        thisRenderer.material = oMat;
+        theBoard.SetMatrix(arrayPos.x, arrayPos.y, 2);
+    }
+}
